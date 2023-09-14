9386b3545ab5e5e0ca34f817338acc2aa5526011
Marc Alessi <marc.alessi@colostate.edu>
2023-09-14 11:57:36 -0600

    main workflow and data prep HW1 edit

--- HW1/RF_cong_dist_pred.py
+++ HW1/RF_cong_dist_pred.py
@@ -1,1 +1,83 @@
-# hi
+# RF workflow for prediction of election outcome for
+# each congressional district
+
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import export_graphviz
+from sklearn.inspection import permutation_importance
+
+#---------------------Load data----------------------------------
+
+# Read csv file
+path = '/Users/marcalessi/Documents/code/ats780/data'
+features = pd.read_csv(path + '/combined_years_census_data_cong_dist.csv')
+
+# Remove predictand from predictors and description column
+features = features.drop(['Geographic Area Name', 'Result'], axis=1)
+feature_list = list(features.columns)
+
+train_features = np.load('npys/train_features.npy')
+test_features = np.load('npys/test_features.npy')
+train_labels = np.load('npys/train_labels.npy')
+test_labels = np.load('npys/test_labels.npy')
+
+
+#----------------------Establish baseline-------------------------
+
+# The baseline predictions here are the 2019 Cook Paristan Voting
+# Index (Cook PVI) values (here set as DEM=1 or REP=-1)
+
+baseline_preds = test_features[:, feature_list.index('2019 Cook PVI')]
+
+# Baseline errors (mean absolute error)
+mae_baseline_errors = abs(baseline_preds - test_labels)
+print('Baseline error (MAE): ', round(np.mean(mae_baseline_errors), 2))
+
+
+#--------------------------Train model-----------------------------
+
+# Tunable parameters
+
+num_trees = np.arange(2, 50, 2)
+tree_depth = None
+node_split = 2
+leaf_samples = 1
+RAND_STATE = 42
+
+error = []
+
+for i in num_trees:
+	print(i)
+	rf = RandomForestRegressor(n_estimators = int(i),
+					random_state = RAND_STATE,
+					min_samples_split = node_split,
+					min_samples_leaf = leaf_samples,
+					max_depth = tree_depth)
+	
+	# Train the model
+	
+	rf.fit(train_features, train_labels)
+	
+	
+	#--------------------------Make predictions-------------------------
+	
+	predictions = rf.predict(test_features)
+	
+	# What does the MAE look like for our RF model?
+	mae_errors = abs(predictions - test_labels)
+	
+	print('Error (MAE): ', round(np.mean(mae_errors), 2))
+	error.append(round(np.mean(mae_errors), 2))
+
+print(error)
+
+fig, ax = plt.subplots()
+
+ax.plot(np.arange(2, 50, 2), error)
+
+plt.savefig('plots/num_trees_mae.png', dpi=300)
--- HW1/data_prep.py
+++ HW1/data_prep.py
@@ -2,13 +2,8 @@
 
 import glob
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-import sklearn
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.tree import export_graphviz
-from sklearn.inspection import permutation_importance
 
 #------------------Load data-------------------------
 
@@ -25,6 +20,13 @@ print(features.describe())
 # predictand
 predictand = np.array(features['Result'])
 
+# Replace "DEM" with 1s and "REP" with -1s in Result column
+mapping = {'REP': -1, 'DEM': 1, 'EVEN': 0}
+
+replace_func = np.vectorize(lambda x: mapping.get(x, x)) # apply mapping to each element
+
+predictand = replace_func(predictand)
+
 # Remove predictand from predictors and description column
 features = features.drop(['Geographic Area Name', 'Result'], axis=1)
 
@@ -34,6 +36,10 @@ feature_list = list(features.columns)
 # Convert to array
 features = np.array(features)
 
+# Replace "DEM" with 1s and "REP" with -1s in Cook PVI (baseline) column
+features = replace_func(features)
+print(features[:,-1])
+
 
 #---------Splitting training/testing datasets---------
 
@@ -42,7 +48,14 @@ split_size = 0.25
 train_features, test_features, train_labels, test_labels =\
 	train_test_split(features, predictand, test_size=split_size, random_state=42)
 
+np.save('npys/train_features', train_features)
+np.save('npys/test_features', test_features)
+np.save('npys/train_labels', train_labels)
+np.save('npys/test_labels', test_labels)
+np.save('npys/feature_list', feature_list)
+
 print('Training Features Shape:', train_features.shape)
 print('Training Labels Shape:', train_labels.shape)
 print('Testing Features Shape:', test_features.shape)
 print('Testing Labels Shape:', test_labels.shape)
+
