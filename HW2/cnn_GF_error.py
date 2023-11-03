6c6664afa3998e3634de7c2df4da4537096e63c3
Marc Alessi <marc.alessi@colostate.edu>
2023-11-03 13:26:19 -0600

    update 11/3

--- HW2/cnn_GF_error.py
+++ HW2/cnn_GF_error.py
@@ -19,5 +19,26 @@ import matplotlib.pyplot as plt
 response = np.load('data/pr_hist_rcp85_response.npy') # 100 ens members, 250 years
 output = np.load('data/pr_hist_rcp85_mpige.npy') 
 
-predictor = np.load('data/tos_hist_rcp85.npy') # 100 ens, 250 yrs, 96 lat, 192 lon
+predictor = np.load('data/rcp85_tos.npy') # 100 ens, 250 yrs, 96 lat, 192 lon
 predictand = response - output
+
+# Split data into training, validation, and testing
+# split along ensemble members (60 for training, 20 for validation, 20 for testing)
+
+Xtrain = predictor[0:60,:,:,:]
+Xval = predictor[60:80,:,:,:]
+Xtest = predictor[80:,:,:,:]
+
+Ytrain = predictand[0:60,:]
+Yval = predictand[60:80,:]
+Ytest = predictand[80:,:]
+
+print('Shapes:')
+print('  Xtrain: ', Xtrain.shape)
+print('  Xval: ', Xval.shape)
+print('  Xtest: ', Xtest.shape)
+
+print('  Ytrain: ', Ytrain.shape)
+print('  Yval: ', Yval.shape)
+print('  Ytest: ', Ytest.shape)
+
